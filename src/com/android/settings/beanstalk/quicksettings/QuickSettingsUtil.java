4388961efb7f5cf1ad511bd00fef699edc463dd0
ILWT <tsubus@lagat.org>
2014-01-12 13:50:22 +0100

    Add Fast Charge QS Tile [2/2]

--- src/com/android/settings/beanstalk/quicksettings/QuickSettingsUtil.java
+++ src/com/android/settings/beanstalk/quicksettings/QuickSettingsUtil.java
@@ -59,6 +59,7 @@ import static com.android.internal.util.beanstalk.QSConstants.TILE_WIFI;
 import static com.android.internal.util.beanstalk.QSConstants.TILE_WIFIAP;
 import static com.android.internal.util.beanstalk.QSConstants.TILE_REBOOT;
 import static com.android.internal.util.beanstalk.QSConstants.TILE_INTERNALMEMORY;
+import static com.android.internal.util.beanstalk.QSConstants.TILE_FCHARGE;
 
 import android.content.ContentResolver;
 import android.content.Context;
@@ -183,6 +184,9 @@ public class QuickSettingsUtil {
 	registerTile(new QuickSettingsUtil.TileInfo(
                 TILE_INTERNALMEMORY, R.string.internal_memory_tile,
                 "com.android.systemui:drawable/ic_qs_internalmemory"));
+        registerTile(new QuickSettingsUtil.TileInfo(
+                TILE_FCHARGE, R.string.title_tile_fcharge,
+                "com.android.systemui:drawable/ic_qs_fcharge_off"));
     }
 
     private static void registerTile(QuickSettingsUtil.TileInfo info) {
@@ -238,6 +242,10 @@ public class QuickSettingsUtil {
         // Don't show the Camera tile if the device has no cameras
         if (!DeviceUtils.deviceSupportsCamera()) {
             removeTile(TILE_CAMERA);
+
+        // Don't show the Fast charge tile if not supported by kernel
+        if (!DeviceUtils.fchargeEnabled(context)) {
+            removeTile(TILE_FCHARGE);
         }
 
     }
